a20c4e177cced7b83deb81b0920c41f68b644fa6
oscarvela <Oscar.VelaGomez@verisk.com>
2026-03-12 14:28:04 +0100

    Ejercicio 2 de la primera entrega terminado. He usado la clase Math para obtener el mayor, aunque se podría hacer mediante comparación en sentencias if

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -12,6 +12,15 @@
             Console.WriteLine("Ingrese el nombre de su ciudad: ");
             string? ciudad = Console.ReadLine();
             Console.WriteLine($"Te llamas {nombre}, tienes {edad} años. Bienvenido a {ciudad}.");
+
+
+
+            // EJERCICIO 2
+            Console.WriteLine("\nEscriba un número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba otro número: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"El número mayor es el {Math.Max(num1, num2)}");
         }
     }
 }
