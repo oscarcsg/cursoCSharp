436108c63c66144ad542f84dd070ad6af9317335
oscarvela <Oscar.VelaGomez@verisk.com>
2026-03-12 14:24:07 +0100

    Primer ejercicio de la primera entrega hecho

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // EJERCICIO 1
+            Console.WriteLine("Ingrese su nombre: ");
+            string? nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el nombre de su ciudad: ");
+            string? ciudad = Console.ReadLine();
+            Console.WriteLine($"Te llamas {nombre}, tienes {edad} años. Bienvenido a {ciudad}.");
         }
     }
 }
