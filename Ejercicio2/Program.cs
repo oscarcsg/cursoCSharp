d4a8b6210c91e174beaaac732e5e68c6eddea374
oscarvela <Oscar.VelaGomez@verisk.com>
2026-03-16 11:08:33 +0100

    Añadido un sistema básico de número máximo de intentos, así como un método que se asegura de que el número a adivinar esté entre 1 y 100 (ambos incluídos). También he hecho la clase excepción personalizada, pero no considero que sea una buena práctica usarla en este proyecto específico porque obliga a cortar el flujo cada vez que el número introducido es mayor o menos al generado. Ahora voy a añadir más complejidad mediante niveles de dificultad.

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -2,13 +2,26 @@
 {
     internal class Program
     {
+
+        // Creo la excepción , pero no la voy a usar porque no creo que tenga mucho sentido cortar el flujo cada vez que
+        // el usuario escriba un número más grande o pequeño que el número a adivinar.
+        public class NumeroException : Exception
+        {
+            public NumeroException(string msg) : base(msg) { }
+        }
+
         static void Main(string[] args)
         {
             // Generar el número a encontrar
-            byte aleatorio = (byte)new Random().Next();
+            byte aleatorio = crearNumero();
+
+            const byte INTENTOS = 5;
+            byte currentIntentos = 0;
 
             byte user = 0;
 
+
+            Console.WriteLine("--- BIENVENIDO A ADIVINA EL NÚMERO. ---\nAdivina el número entre el 1 y el 100.\n");
             do
             {
                 Console.Write($"Escriba un número: ");
@@ -19,13 +32,17 @@
                 }
                 catch (Exception)
                 {
-                    //throw new Exception("El valor introducido no es válido, debe ser un valor numérico entre 0 y 256.");
+                    //throw new Exception("El valor introducido no es válido, debe ser un valor numérico entre 1 y 100.");
                     // No lanzo la excepción porque en este ejemplo concreto de programa no tendría sentido cortar el flujo del programa
-                    Console.WriteLine("El valor introducido no es válido, debe ser un valor numérico entre 0 y 256.");
+                    Console.WriteLine("El valor introducido no es válido, debe ser un valor numérico entre 1 y 100.");
                     continue;
                 }
 
                 // No hace falta hacer comprobación de si el valor introducido está en los limites del byte porque eso ya lo controla try
+
+                currentIntentos++;
+
+                if (currentIntentos == INTENTOS) break;
 
                 // Mensajes de numero grande o pequeño
                 if (user > aleatorio) Console.WriteLine("El número es más pequeño.");
@@ -36,7 +53,21 @@
             // Esta comprobación no sería necesaria porque el bucle termina cuando el usuario ha acertado el número
             // pero la voy a hacer por robusted
             if (user == aleatorio) Console.WriteLine("¡¡¡ENHORABUENA!!! Has acertado el número.");
-            else Console.WriteLine("Has perdido."); // Este mensaje (por ahora) no sería necesario
+            else Console.WriteLine($"Has perdido. El número era {aleatorio}."); // Este mensaje (por ahora) no sería necesario
+        }
+
+        private static byte crearNumero()
+        {
+            Random rand = new Random();
+            byte num = 0;
+
+            do
+            {
+                num = (byte) rand.Next();
+            }
+            while (num == 0 || num > 100);
+
+            return num;
         }
     }
 }
