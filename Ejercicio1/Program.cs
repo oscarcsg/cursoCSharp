bafcf20f8ccefe7677ce2feda90cb230709f4791
oscarvela <Oscar.VelaGomez@verisk.com>
2026-03-16 09:09:33 +0100

    Ejercicio 6 hecho y comentado

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -60,6 +60,45 @@
             var rand = new Random();
             for (int i = 0; i < cantidad; i++) arr[i] = rand.Next();
             Console.WriteLine(string.Join(", ", arr));
+
+
+
+            // EJERCICIO 6
+            Console.WriteLine("\nIntroduzca cuántas filas quiere que tenga la pirámide: ");
+            int filas = int.Parse(Console.ReadLine());
+            Console.WriteLine("¿Qué caracter quiere usar para la pirámide?");
+            string? c = Console.ReadLine();
+
+            int cont = 0;
+            Console.WriteLine("\n");
+            // Bucle para mover entre filas
+            for (int i = 0; i < filas; i++)
+            {
+                // Aumentar el contador de cuántas columnas debe tener cada fila en 1
+                cont++;
+
+                // Bucle para mover entre columnas de una fila
+                for (int j = 0; j < cont; j++)
+                {
+                    // Controlar cuándo estamos en la última fila para escribir la línea completa
+                    if (i == filas-1)
+                    {
+                        Console.Write(c);
+                        /* Pasar a la siguiente columna (j) para evitar hacer comprobaciones de cuándo la columna actual
+                         * es la primera o la última de la fila
+                         */
+                        continue;
+                    }
+
+                    // Si la columna actual es EXCLUSIVAMENTE la primera o la última de la fila, entonces pinta el caracter
+                    if (j == 0 || j == i) Console.Write(c);
+                    // Si no, pinta un espacio en blanco
+                    else Console.Write(" ");
+
+                }
+                // Salto de línea para empezar en la siguiente fila
+                Console.WriteLine();
+            }
         }
     }
 }
