d94729b9406a501219d7b78e8dd9358d74d508bb
oscarvela <Oscar.VelaGomez@verisk.com>
2026-03-17 08:04:28 +0100

    Ejercicio de los libros leídos y no leídos hecho.

---
+++ Ejercicio3/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercicio3
+{
+    internal class Book
+    {
+        public string? _Title { get; set; }
+        public bool _IsRead { get; set; }
+
+        public Book() { }
+
+        public Book(string? title)
+        {
+            _Title = title;
+        }
+
+        public Book(string? title, bool isRead) : this(title)
+        {
+            _IsRead = isRead;
+        }
+    }
+}
--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Book> books = CreateBookList();
+            Console.WriteLine(IsBookRead(books, "Libro 1")); // false
+            Console.WriteLine(IsBookRead(books, "Libro 6")); // true
+            Console.WriteLine(IsBookRead(books, "Libro 8")); // false (no existe)
+        }
+
+        private static List<Book> CreateBookList()
+        {
+            List<Book> list = new List<Book>();
+            for (int i = 1; i <= 5; i++) list.Add(new Book($"Libro {i}", false));
+
+            list.Add(new Book("Libro 6", true));
+
+            return list;
+        }
+
+        private static bool IsBookRead(List<Book> bookList, string? bookTitle)
+        {
+            foreach (Book b in bookList)
+            {
+                if (bookTitle.Equals(b._Title)) return b._IsRead;
+            }
+            // False si el libro no se ha encontrado (no existe)
+            return false;
         }
     }
 }
