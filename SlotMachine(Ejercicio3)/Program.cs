174dfce3f74371a48a31d5eab0b95337538fc6f7
oscarvela <Oscar.VelaGomez@verisk.com>
2026-03-17 08:09:21 +0100

    Creado el proyecto para hacer el punto 2 del ejercicio 3 (la máquina tragaperras)

---
+++ SlotMachine(Ejercicio3)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SlotMachine_Ejercicio3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
