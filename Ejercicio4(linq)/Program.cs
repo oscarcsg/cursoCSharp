ef297fcbe08fb228bfbc06255bafba152310cc47
oscarvela <Oscar.VelaGomez@verisk.com>
2026-03-20 12:10:42 +0100

    Proyecto para el ejercicio 4 hecho.

---
+++ Ejercicio4(linq)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio4_linq_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
